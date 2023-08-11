3ee9159a89acd977088dd8a3f241427556afb856
hyerimmy <heyrim2010@naver.com>
2023-08-11 18:09:40 +0900

    [feat] 파일 기본 구조 작성

---
+++ Programmers/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(solution("ㄴ"));
+    }
+    public static String solution(String s) {
+        return s;
+    }
+}
