dbea02a54099b68824f3354dcfa715054f95430f
hyerimmy <heyrim2010@naver.com>
2023-09-16 23:49:35 +0900

    2023.09.16 sat

---
+++ Code/Programmers/Lv0_문자열섞기.java
@@ -0,0 +1,26 @@
+package Programmers;
+
+import java.util.Scanner;
+
+/**
+ * @title Lv0 문자열 섞기
+ * @link https://school.programmers.co.kr/learn/courses/30/lessons/181942
+ * @date 2023.09.16
+ * @notes
+ * - Java 문법 공부용, 푼 문제중 한 개만 git에 등록 <br>
+ * */
+
+class Lv0_문자열섞기 {
+
+    public static String main(String str1, String str2) {
+        String answer = "";
+        for(int i=0; i<str1.length();i++){
+            answer += str1.charAt(i);
+            answer += str2.charAt(i);
+        }
+        return answer;
+    }
+
+}
+
+
