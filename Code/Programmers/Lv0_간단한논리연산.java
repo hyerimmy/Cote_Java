1e11040f9c35670018d32be8f9bfefc9ce360ef5
hyerimmy <heyrim2010@naver.com>
2023-09-29 21:04:31 +0900

    2023.09.29 fri

---
+++ Code/Programmers/Lv0_간단한논리연산.java
@@ -0,0 +1,24 @@
+package Programmers;
+
+import java.util.Scanner;
+
+/**
+ * @title Lv0 홀짝구분하기
+ * @link https://school.programmers.co.kr/learn/courses/30/lessons/181917
+ * @date 2023.09.29
+ * @notes
+ * - Java 문법 공부용 <br>
+ * */
+
+class Lv0_간단한논리연산 {
+
+    public static void main(String[] args) {
+    }
+
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        return (x1|x2)&&(x3|x4);
+    }
+
+}
+
+
