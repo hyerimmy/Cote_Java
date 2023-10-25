7e273606bdc68ac28ebfa273c4b0b5b0e978464f
hyerimmy <heyrim2010@naver.com>
2023-10-25 20:45:14 +0900

    2023.10.25 wed

--- Code/BOJ/failed_g5_2493_탑.java
+++ Code/BOJ/failed_g5_2493_탑.java
@@ -13,6 +13,7 @@ import java.util.*;
  * - 시간초과 문제 제약사항의 범위가 크기 때문에 (500,000) 시간복잡도 고려해야함.
  * - 메모리초과 -> bufferdReader랑 Stringtokenizer!!! 쓰는 습관 만들자.
  * - 큐가 아니라 스택 활용하는 문제!!! 문제 이해 잘 할 수 있도록 머리굴리자.
+ * - 시간초과 -> 문제 유형에 익숙해지자
  */
 
 public class failed_g5_2493_탑 {
@@ -22,27 +23,36 @@ public class failed_g5_2493_탑 {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int[] heightA = new int[N];
-        int[] answerA = new int[N];
 
+        Stack<Top> S = new Stack<>();
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
-            heightA[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Stack<Integer> S = new Stack<>();
-        for(int i=N-1; i>=0; i--){
-            while(!S.empty() && heightA[S.peek()]<heightA[i]){
-                answerA[S.pop()] = i+1;
+            int height = Integer.parseInt(st.nextToken());
+            while(true){
+                if(S.empty()){
+                    System.out.print("0 ");
+                    break;
+                }
+                if(S.peek().height < height){
+                    S.pop();
+                }
+                else{
+                    System.out.print(S.peek().idx+" ");
+                    break;
+                }
             }
-            S.push(i);
+            S.push(new Top(i+1, height));
         }
+    }
+}
 
-        for(int i=0; i<N; i++){
-            System.out.print(answerA[i]);
-            if(i!=N-1) System.out.print(" ");
-        }
+class Top{
+    int idx;
+    int height;
 
+    public Top(int idx, int height){
+        this.idx=idx;
+        this.height=height;
     }
 }
 
