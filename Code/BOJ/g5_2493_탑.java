1eec21b8a22ece976bf7974b963a3fb78210988c
hyerimmy <heyrim2010@naver.com>
2023-10-25 21:03:27 +0900

    2023.10.25 wed

--- Code/BOJ/g5_2493_탑.java
+++ Code/BOJ/g5_2493_탑.java
@@ -16,7 +16,7 @@ import java.util.*;
  * - 시간초과 -> 문제 유형에 익숙해지자
  */
 
-public class failed_g5_2493_탑 {
+public class g5_2493_탑 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
