1e12b9f1de8fa18debbb9043da55eb8cb51b737c
hyerimmy <heyrim2010@naver.com>
2023-10-29 22:48:54 +0900

    2023.10.29 sun

--- Code/BOJ/failed_g4_17281_야구공이모지.java
+++ Code/BOJ/failed_g4_17281_야구공이모지.java
@@ -13,9 +13,11 @@ import java.util.*;
  * - 9명인데 4번타자 고정, 8명 정렬 경우의 수는 40320, N이닝이니까 -> 40320N
  * - 모든 경우의 수 계산해서 최대값 찾아야 한다.
  * - 왜 에러나는지 모르겠다~ 아~
+ * - 가장 좋아하는 선수인 1번 선수를 4번 타자로 미리 결정 -> 1번 사람을 4번째로 (문제 해석 잘못해서 오류났었던)
+ * - ㅎㅎ 시간초과? 장난하나..
  */
 
-public class g4_17281_야구공이모지 {
+public class failed_g4_17281_야구공이모지 {
     public static int N;
     public static int[][] S;
 
@@ -38,15 +40,24 @@ public class g4_17281_야구공이모지 {
         Q.add(new PlayerSetting());
         while(!Q.isEmpty()){
             PlayerSetting ps = Q.poll();
-            if(ps.playerResult.size()==9){
+            if(ps.playerOrder.size()==9){
                 // play and update maxScore
-                int playScore = getPlayScore(ps.playerResult);
-                if(maxScore < getPlayScore(ps.playerResult)) maxScore=playScore;
+                int playScore = getPlayScore(ps.playerOrder);
+                if(playScore==48){
+                    getPlayScore(ps.playerOrder);
+                }
+                if(maxScore < getPlayScore(ps.playerOrder)) maxScore=playScore;
             }
             else{
-                for(int playerIdx=0; playerIdx<9; playerIdx++){
-                    if(!ps.playerUsed[playerIdx]){
-                        Q.add(ps.setPlayer(playerIdx));
+                //1번 사람을 4번째로
+                if(ps.playerOrder.size()==3){
+                    Q.add(ps.setPlayerOrder(0));
+                }
+                else{
+                    for(int playerIdx=0; playerIdx<9; playerIdx++){
+                        if(!ps.playerUsed[playerIdx]){
+                            Q.add(ps.setPlayerOrder(playerIdx));
+                        }
                     }
                 }
             }
@@ -58,29 +69,31 @@ public class g4_17281_야구공이모지 {
     public static int getPlayScore(ArrayList<Integer> P){
         int scoreSum = 0;
         int playerIdx = 0;
+        Queue<Integer> ru = new LinkedList<>();
         for(int ining=0; ining<N; ining++){
             int outCnt = 0;
-            playerIdx = 0;
-            HashMap<Integer, Integer> ru = new HashMap<>();
+            ru.clear();
             while(outCnt<3){
                 int player = P.get(playerIdx);
                 int score = S[ining][player];
 
                 if (score == 0) { //아웃
                     outCnt++;
-                } else if(score == 4){
+                } else if(score == 4){ //홈런
                     scoreSum+=ru.size()+1;
                     ru.clear();
                 }else {
-                    for (int ruPlayer : ru.keySet()) {
-                        if (ru.get(ruPlayer) + score > 3) {
-                            ru.remove(ruPlayer);
+                    int ruSize = ru.size();
+                    for (int i=0; i<ruSize; i++) {
+                        int ruScore = ru.poll();
+                        if (ruScore + score > 3) {
                             scoreSum++;
-                        } else {
-                            ru.put(ru.get(ruPlayer), ru.get(ruPlayer) + score);
+                        }
+                        else {
+                            ru.add(ruScore + score);
                         }
                     }
-                    ru.put(player, score);
+                    ru.add(score);
                 }
                 playerIdx++;
                 if(playerIdx==9) playerIdx=0;
@@ -92,26 +105,26 @@ public class g4_17281_야구공이모지 {
 
 class PlayerSetting{
     boolean[] playerUsed;
-    ArrayList<Integer> playerResult;
+    ArrayList<Integer> playerOrder;
 
     public PlayerSetting(){
         playerUsed = new boolean[9];
-        playerResult = new ArrayList<>();
+        playerOrder = new ArrayList<>();
 
-        playerUsed[3] = true;
-        playerResult.add(3);
+        //1번 사람을 4번째로
+        playerUsed[0] = true;
     }
 
-    public PlayerSetting(boolean[] playerUsed, ArrayList<Integer> playerResult){
+    public PlayerSetting(boolean[] playerUsed, ArrayList<Integer> playerOrder){
         this.playerUsed = playerUsed;
-        this.playerResult = playerResult;
+        this.playerOrder = playerOrder;
     }
 
-    public PlayerSetting setPlayer(int playerIdx){
+    public PlayerSetting setPlayerOrder(int playerIdx){
         boolean[] newPlayerUsed = playerUsed.clone();
-        ArrayList<Integer> newPlayerResult = new ArrayList<>(playerResult);
+        ArrayList<Integer> newPlayerOrder = new ArrayList<>(playerOrder);
         newPlayerUsed[playerIdx] = true;
-        newPlayerResult.add(playerIdx);
-        return new PlayerSetting(newPlayerUsed, newPlayerResult);
+        newPlayerOrder.add(playerIdx);
+        return new PlayerSetting(newPlayerUsed, newPlayerOrder);
     }
 }
