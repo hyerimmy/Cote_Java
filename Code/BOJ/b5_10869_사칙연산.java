33287e3aa5bc0e1c4b34b826f7cc611580fa243c
hyerimmy <heyrim2010@naver.com>
2023-10-31 19:22:12 +0900

    2023.10.31 tue

---
+++ Code/BOJ/b5_10869_사칙연산.java
@@ -0,0 +1,30 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @link https://www.acmicpc.net/problem
+ * @date 2023.10.31.
+ * @notes - 현대 기출<br>
+ */
+
+public class b5_10869_사칙연산 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
+
