66dfad07b472aec1e090f45e8b98d8bfd7af0282
hyerimmy <heyrim2010@naver.com>
2023-11-22 23:04:45 +0900

    2023.11.21 tue

--- Code/BOJ/s3_2579_계단오르기.java
+++ Code/BOJ/s3_2579_계단오르기.java
@@ -17,17 +17,17 @@ public class s3_2579_계단오르기 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[] stair = new int[N+1];
-        for(int i=1; i<=N; i++){
+        int[] stair = new int[301];
+        for (int i = 1; i <= N; i++) {
             stair[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] dp = new int[N+1];
+        int[] dp = new int[301];
         dp[0] = 0;
         dp[1] = stair[1];
         dp[2] = dp[1] + stair[2];
         dp[3] = Math.max(stair[1], stair[2]) + stair[3];
-        for(int i=4; i<=N; i++){
+        for (int i = 4; i <= N; i++) {
             dp[i] = Math.max(stair[i - 1] + dp[i - 3], dp[i - 2]) + stair[i];
         }
         System.out.println(dp[N]);
