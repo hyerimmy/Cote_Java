e646f77b96bb8cf625a0dd3ae98f4377b3251844
hyerimmy <heyrim2010@naver.com>
2024-01-01 23:19:58 +0900

    2024.01.01 mon

---
+++ Code/BOJ/g5_1334_다음팰린드롬수.java
@@ -0,0 +1,31 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/**
+ * @link https://www.acmicpc.net/problem
+ * @date 2023.01.01
+ * @notes - 구현 <br>
+ */
+
+public class g5_1334_다음팰린드롬수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        while(true){
+            // 다음 수로 만들기
+            N++;
+
+            // 팰린드롬 검사
+            for(int i=0; i<)
+        }
+
+    }
+}
