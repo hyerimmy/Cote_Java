76d7c36fed656b7743bc4e9000b5e90c76a58cea
hyerimmy <heyrim2010@naver.com>
2024-01-11 23:21:04 +0900

    2024.01.11 thu

---
+++ Code/BOJ/s4_1026_보물.java
@@ -0,0 +1,43 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * @link https://www.acmicpc.net/problem
+ * @date 2024.01.11
+ * @notes - 그리디 <br>
+ */
+
+public class s4_1026_보물 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] B = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int result = 0;
+        for(int i=0; i<N; i++){
+            result += A[i] * B[N-i-1];
+        }
+
+        System.out.println(result);
+    }
+}
