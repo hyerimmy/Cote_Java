f0e92ceb86e62832462990eab95b9e58b644712d
hyerimmy <hyerim2000@swu.ac.kr>
2024-01-20 23:09:18 +0900

    2024.01.20 sat

--- Code/Programmers/failed_Lv2_조이스틱.java
+++ Code/Programmers/failed_Lv2_조이스틱.java
@@ -6,11 +6,9 @@ import java.util.*;
  * @title Lv2 소수찾기
  * @link https://school.programmers.co.kr/learn/courses/30/lessons/42860
  * @date 2024.01.18
- * @notes
- * - 그리디 고득점 kit <br>
+ * @notes - 그리디 고득점 kit <br>
  * - 왼쪽, 오른쪽 이동 카운팅 시 경우의 수 누락!!!!!!! <br>
- *
- * */
+ */
 
 class failed_Lv2_조이스틱 {
     public static void main(String[] args) {
@@ -19,7 +17,7 @@ class failed_Lv2_조이스틱 {
     public int solution(String name) {
         int answer = 0;
         List<Integer> changePoint = new ArrayList<>();
-        for (int i = 0; i < name.length(); i++){
+        for (int i = 0; i < name.length(); i++) {
             // 변경할 필요 없다면 건너뛰기
             if (name.charAt(i) == 'A')
                 continue;
@@ -36,7 +34,7 @@ class failed_Lv2_조이스틱 {
 
         // 왼쪽 오른쪽 카운트
         Collections.sort(changePoint);
-        if (changePoint.size() > 0){
+        if (changePoint.size() > 0) {
             int leftCount = name.length() - changePoint.get(0);
             int rightCount = changePoint.get(changePoint.size() - 1);
             answer += Math.min(leftCount, rightCount);
