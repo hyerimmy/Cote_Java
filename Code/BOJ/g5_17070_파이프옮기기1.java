e1315a92b03b6ae499cdbf8f30b49bffd67741a3
hyerimmy <hyerim2000@swu.ac.kr>
2024-02-16 23:35:45 +0900

    2024.02.16 fri

---
+++ Code/BOJ/g5_17070_파이프옮기기1.java
@@ -0,0 +1,18 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @link https://www.acmicpc.net/problem
+ * @date 2024.02.16.
+ * @notes -  <br>
+ */
+
+public class g5_17070_파이프옮기기1 {
+
+    public static void main(String[] args) throws IOException {
+    }
+}
+
