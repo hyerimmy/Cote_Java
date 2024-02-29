63ec1035f44a19e2259e6d8252a40e8f5cf6def6
hyerimmy <hyerim2000@swu.ac.kr>
2024-02-29 23:10:49 +0900

    2024.02.29 thu

--- Code/Programmers/failed_Lv2_2xn타일링.java
+++ Code/Programmers/failed_Lv2_2xn타일링.java
@@ -4,13 +4,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * @title Lv2 n^2배열
- * @link https://school.programmers.co.kr/learn/courses/30/lessons/87390
- * @date 2023.09.25
- * @notes
- * - long형으로 입력받는 문제일 때는 int형 변환 잘 하기!!!<br>
- * - 어디서 int로 형변환 하느냐에 따라서 반올림때문에 정확도 실패가 될 수 있음!!<br>
- * */
+ * @link https://school.programmers.co.kr/learn/courses/30/lessons/12900
+ * @date 2024.02.29
+ * @notes - 시간초과 <br/>
+ */
 
 class failed_Lv2_2xn타일링 {
     public static void main(String[] args) {
@@ -20,19 +17,19 @@ class failed_Lv2_2xn타일링 {
 
     public static int solution(int n) {
         getCnt(n);
-        return answer%1000000007;
+        return answer % 1000000007;
     }
 
-    public static void getCnt(int n){
+    public static void getCnt(int n) {
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(n);
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int size = queue.poll();
-            if(size<2) answer++;
+            if (size < 2) answer++;
             else {
-                queue.offer(size-1);
-                queue.offer(size-2);
+                queue.offer(size - 1);
+                queue.offer(size - 2);
             }
         }
     }
