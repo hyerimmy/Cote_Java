b4264ec01da0f0a51d14c835aa632c7141285a6b
hyerimmy <hyerim2000@swu.ac.kr>
2024-03-19 23:53:24 +0900

    2024.03.19 tue

--- Code/BOJ/_failed_g3_11049_행렬곱셈순서.java
+++ Code/BOJ/_failed_g3_11049_행렬곱셈순서.java
@@ -3,6 +3,8 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /**
@@ -11,12 +13,18 @@ import java.util.StringTokenizer;
  * @notes - 퐈이팅~ 해야쥐~<br>
  */
 
-public class g3_11049_행렬곱셈순서 {
+public class _failed_g3_11049_행렬곱셈순서 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
 
+        List<int[]> inputs = new ArrayList<>();
+        for (int n = 0; n < N; n++) {
+            st = new StringTokenizer(br.readLine());
+            inputs.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+        }
 
     }
 }
