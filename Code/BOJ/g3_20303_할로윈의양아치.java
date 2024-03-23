ad1c10044f4d7b201b1a77d10b7d25c2b41771e2
hyerimmy <hyerim2000@swu.ac.kr>
2024-03-23 21:01:20 +0900

    2024.03.23 sat

---
+++ Code/BOJ/g3_20303_할로윈의양아치.java
@@ -0,0 +1,126 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * @link https://www.acmicpc.net/problem/
+ * @date 2024.03.23
+ * @notes - 골3 도전<br>
+ * - 한 번에 풀어보자. (1. BFS -> 2. 배낭문제DP)
+ * - 망할 배낭문제!!!! 바보야 배낭문제 알고리즘 제대로 알지도 못했으면서 왜 아는줄 알았던거니~~~ 기억해
+ */
+
+public class g3_20303_할로윈의양아치 {
+    public static int N, M, K;
+    public static int[] candies;
+
+    public static List<List<Integer>> relations = new ArrayList<>();
+    public static List<Friends> friendsList = new ArrayList<>();
+    public static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        candies = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int n = 1; n <= N; n++) {
+//            Random rd = new Random();
+//            candies[n] = rd.nextInt(10000) + 1;
+
+            candies[n] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int n = 0; n <= N; n++) {
+            relations.add(new ArrayList<>());
+        }
+
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+//            Random rd = new Random();
+//            int a = rd.nextInt(N) + 1;
+//            int b = rd.nextInt(N) + 1;
+
+            relations.get(a).add(b);
+            relations.get(b).add(a);
+        }
+
+        // BFS
+        visited = new boolean[N + 1];
+        for (int n = 1; n <= N; n++) {
+            if (!visited[n]) BFS(n);
+        }
+
+        // DP
+        int friendsSize = friendsList.size();
+
+        int[][] dp = new int[friendsSize + 1][K];
+
+        for (int f = 1; f <= friendsSize; f++) {
+            Friends friends = friendsList.get(f - 1);
+//            System.out.println(friends);
+
+            for (int max = 1; max <= K - 1; max++) {
+                // 1. 친구무리를 넣지 않는 경우
+                dp[f][max] = dp[f - 1][max];
+
+                // 2. 친구무리를 넣는 경우
+                if (friends.friendCount <= max) {
+                    dp[f][max] = Math.max(dp[f][max], friends.candyCount + dp[f - 1][max - friends.friendCount]);
+                }
+            }
+
+//            System.out.println(Arrays.toString(dp[f]));
+        }
+
+
+        // 출력
+        System.out.println(dp[friendsSize][K - 1]);
+    }
+
+    public static void BFS(int startN) {
+        Queue<Integer> Q = new LinkedList<>();
+
+        visited[startN] = true;
+        int friendCount = 1;
+        int candyCount = candies[startN];
+        Q.add(startN);
+
+        while (!Q.isEmpty()) {
+            int n = Q.poll();
+
+            for (int friend : relations.get(n)) {
+                if (visited[friend]) {
+                    continue;
+                }
+
+                visited[friend] = true;
+                friendCount++;
+                candyCount += candies[friend];
+                Q.add(friend);
+            }
+
+        }
+
+        friendsList.add(new Friends(friendCount, candyCount));
+    }
+
+    private static class Friends {
+        int friendCount;
+        int candyCount;
+
+        public Friends(int friendCount, int candies) {
+            this.friendCount = friendCount;
+            this.candyCount = candies;
+        }
+    }
+}
---
+++ Code/BOJ/g5_12865_평범한배낭__배낭문제.java
@@ -0,0 +1,121 @@
+package BOJ;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * @link https://www.acmicpc.net/problem
+ * @date 2023.10.29
+ * @notes - 브루트포스트
+ * - 메모리 초과 : BFS->DFS 변경해보자. : 해결<br>
+ * - 시간 초과 : DFS 호출 시 이전에 확인했던 값은 제외하고 재귀하도록 startIdx 추가 : 7%, 시간초과<br>
+ * - BFS를 제대로 이해하지 못했다.<br>
+ * - 푸는 방법에는 2가지 : (1)백트래킹, (2)동적계획법<br>
+ * - -> (1) 백트래킹 : 물건을 선택하거나 선택하지 않거나 싹 돌아보기
+ * - -> (2) 동적계획법 DP : 각 단계에서 구한 해를 다음 문제에서 재사용
+ * - 동적계획법 사용해서 풀었다. 틀렸다.. -> index의 굴레.. 정신 제대로 차려보자!! 성공.
+ */
+
+//public class g5_12865_평범한배낭__배낭문제 {
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int N = Integer.parseInt(st.nextToken()); //물품수
+//        int K = Integer.parseInt(st.nextToken()); //최대무게
+//
+//        Item[] itemA = new Item[N + 1];
+//        for (int i = 1; i <= N; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            itemA[i] = new Item(
+//                    Integer.parseInt(st.nextToken()),
+//                    Integer.parseInt(st.nextToken())
+//            );
+//        }
+//
+//        int[][] DP = new int[N + 1][K + 1];
+//        for (int k = 1; k <= K; k++) {
+//            for (int i = 1; i <= N; i++) {
+//                // 물건을 넣는 경우
+//                if (itemA[i].w <= k)
+//                    DP[i][k] = itemA[i].v + DP[i - 1][k - itemA[i].w];
+//                // 물건을 넣지 않는 경우
+//                DP[i][k] = Math.max(DP[i][k], DP[i - 1][k]);
+//            }
+//        }
+//        System.out.print(DP[N][K]);
+//    }
+//
+//    public static class Item {
+//        public int w;//무게
+//        public int v;//가치
+//
+//        public Item(int w, int v) {
+//            this.w = w;
+//            this.v = v;
+//        }
+//    }
+//}
+
+public class g5_12865_평범한배낭__배낭문제 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); //물품수
+        int K = Integer.parseInt(st.nextToken()); //최대무게
+
+        // 물건 입력받기
+        List<Item> itemList = new ArrayList<>();
+        for (int n = 0; n < N; n++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            itemList.add(new Item(w, v));
+        }
+
+        // DP
+        int[][] dp = new int[K + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            Item item = itemList.get(i - 1);
+
+            for (int max = 1; max <= K; max++) {
+                // 물건을 넣지 않는 경우
+                dp[max][i] = dp[max][i - 1];
+
+                // 물건을 넣는 경우
+                if (max >= item.w) {
+                    dp[max][i] = Math.max(dp[max][i], item.v + dp[max - item.w][i - 1]);
+                }
+            }
+        }
+
+//        for (int k = 1; k <= K; k++) {
+//            for (int i = 1; i <= N; i++) {
+//                // 물건을 넣는 경우
+//                if (itemA[i].w <= k)
+//                    DP[i][k] = itemA[i].v + DP[i - 1][k - itemA[i].w];
+//                // 물건을 넣지 않는 경우
+//                DP[i][k] = Math.max(DP[i][k], DP[i - 1][k]);
+//            }
+//        }
+
+        // 결과 출력
+        System.out.println(dp[K][N]);
+    }
+
+    public static class Item {
+        public int w;//무게
+        public int v;//가치
+
+        public Item(int w, int v) {
+            this.w = w;
+            this.v = v;
+        }
+    }
+}
