e5a9bbdef252f5cb5368fe82de0f4df53d9e11ca
hyerimmy <hyerim2000@swu.ac.kr>
2024-04-01 18:08:46 +0900

    2024.04.01 mon

--- Code/BOJ/g1_11401_이항계수3.java
+++ Code/BOJ/g1_11401_이항계수3.java
@@ -18,7 +18,7 @@ import java.util.StringTokenizer;
 6C3 = 6*5*4/3*2*1 = 6C2 * 4/3 = 6C2 * (6-3+1)/3
 nCk = nC(k-1) * (n-k+1)/k
  */
-public class _failed_g1_11401_이항계수3 {
+public class g1_11401_이항계수3 {
     public static int DIV = 1_000_000_007;
 
     public static void main(String[] args) throws IOException {
@@ -30,11 +30,23 @@ public class _failed_g1_11401_이항계수3 {
         long[] results = new long[K + 1];
         results[0] = 1;
         for (int k = 1; k <= K; k++) {
-            results[k] = (results[k - 1] * (N - k + 1) / k) % DIV;
+            results[k] = results[k - 1] * (N - k + 1) % DIV * calculatePow(k, DIV - 2) % DIV;
         }
 
 //        System.out.println(Arrays.toString(results));
         System.out.println(results[K]);
+    }
+
+    private static int calculatePow(int a, int b) {
+        if (b == 1) {
+            return a;
+        }
+
+        int resut = calculatePow(a, b / 2);
+        if (b % 2 == 0) {
+            return (int) ((long) resut * resut % DIV);
+        }
+        return (int) ((long) resut * resut % DIV * a % DIV);
     }
 }
 
