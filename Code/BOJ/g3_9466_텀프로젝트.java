4284df8a8c71c38d7fdcd426b64f2c638794c6d6
hyerimmy <hyerim2000@swu.ac.kr>
2024-04-11 00:29:15 +0900

    2024.04.11 thu

--- Code/BOJ/g3_9466_텀프로젝트.java
+++ Code/BOJ/g3_9466_텀프로젝트.java
@@ -9,13 +9,16 @@ import java.util.StringTokenizer;
 /**
  * @link https://www.acmicpc.net/problem/9466
  * @date 2024.04.10
- * @notes - <br>
+ * @notes - 1트(50m) : 79%에서 시간초과 <br>
+ * - 2트(55m) : 성공, visited 배열 통해서 이미 방문한 노드 재방문하지 않도록 처리. <br/>
+ * - 작은 테스트케이스에서는 큰 차이 없어보여도 히든에서 시간초과 날 수 있으니 생각나는 모든 경우는 최대한 제거하도록 할 것..
  */
 
 public class g3_9466_텀프로젝트 {
     public static int N;
     public static int[] wants;
     public static boolean[] canMakeTeam;
+    public static boolean[] visited;
     public static HashSet<Integer> history; // DFS 탐색용
 
     public static void main(String[] args) throws IOException {
@@ -31,10 +34,13 @@ public class g3_9466_텀프로젝트 {
             // 탐색 위한 배열 크기 지정
             wants = new int[N + 1];
             canMakeTeam = new boolean[N + 1];
+            visited = new boolean[N + 1];
 
             // 팀하고 싶은 학생 정보 입력
             st = new StringTokenizer(br.readLine());
             for (int n = 1; n <= N; n++) {
+//                Random rd = new Random();
+//                wants[n] = rd.nextInt(N) + 1;
                 wants[n] = Integer.parseInt(st.nextToken());
             }
 
@@ -62,13 +68,30 @@ public class g3_9466_텀프로젝트 {
     }
 
     private static int DFS(int student) {
+        // 이미 방문한 루트라면 사이클 만들 수 없는 경우뿐
+        if (visited[student])
+            return -1;
+
         history.add(student);
+        visited[student] = true;
         int next = wants[student];
+
+//        System.out.println();
+//        System.out.println("== " + student + " & next : " + next);
+//        System.out.println(history);
+//        System.out.println(Arrays.toString(canMakeTeam));
 
         // 팀이 지금 결성된다면 (next = 팀의 시작점)
         if (history.contains(next)) {
             canMakeTeam[student] = true;
-            return next;
+            if (student == next) // 스스로 팀 결성하고 끝난 경우
+                return -1;
+            return next; // 팀 결성 계속해서 처리해야 하는 경우
+        }
+
+        // 이미 팀 결성한 학생을 원하는 경우라면 팀 결성 불가
+        if (canMakeTeam[next]) {
+            return -1;
         }
 
         // 끝까지 탐색했을 때 팀의 시작점을 target으로 받아옴
