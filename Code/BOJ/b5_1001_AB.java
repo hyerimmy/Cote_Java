d88d902771ed547b7930c6b1a78d0e8089a2bbb4
hyerimmy <hyerim2000@swu.ac.kr>
2024-05-04 23:25:09 +0900

    2024.05.04 sat

---
+++ Code/BOJ/b5_1001_AB.java
@@ -0,0 +1,26 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @link https://www.acmicpc.net/problem/1001
+ * @date 2024.05.03
+ * @notes - <br>
+ */
+
+public class b5_1001_AB {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A - B);
+    }
+}
+
---
+++ Code/BOJ/b5_11382_꼬마정민.java
@@ -0,0 +1,26 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @link https://www.acmicpc.net/problem/11382
+ * @date 2024.05.04
+ * @notes - <br>
+ */
+
+public class b5_11382_꼬마정민 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        System.out.println(A + B + C);
+    }
+}
+
