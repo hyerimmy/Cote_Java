37218bc44544bf8e756eda97f9cbd099a8fe8932
hyerimmy <hyerim2000@swu.ac.kr>
2024-05-10 23:52:23 +0900

    2024.05.10 fri

---
+++ Code/BOJ/s3_1051_숫자정사각형.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @link https://www.acmicpc.net/problem/1051
+ * @date 2024.05.10
+ * @notes - <br>
+ */
+
+public class s3_1051_숫자정사각형 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+}
+
